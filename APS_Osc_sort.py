435b7a05d94ea274109375ef16b8e61f6b9f7840
Aleksey Evdakov <eae_rus@mail.ru>
2024-02-15 23:27:40 +0300

    Первая версия функции сортировки

---
+++ APS_Osc_sort.py
@@ -0,0 +1,71 @@
+import os
+import shutil
+import hashlib
+import json
+
+# Функция для обхода файловой системы
+def copy_cfg_and_dat_files(source_dir, dest_dir):
+    hash_table = {}  # Создаем хэш-таблицу для отслеживания скопированных файлов
+    for root, dirs, files in os.walk(source_dir):  # Итерируемся по всем файлам и директориям в исходной директории
+        for file in files:  # Имя каждого файла
+            if file.endswith(".cfg"):  # Если файл имеет расширение .cfg
+                file_path = os.path.join(root, file)  # Получаем полный путь к cfg файлу
+                dat_file = file[:-4] + ".dat"  # Формируем имя dat файла на основе имени cfg файла
+                dat_file_path = file_path[:-4] + ".dat"  # Получаем полный путь к dat файлу
+                if os.path.exists(dat_file_path):
+                    with open(file_path, 'rb') as f:  # Открываем cfg файл для чтения в бинарном режиме
+                        file_hash = hashlib.md5(f.read()).hexdigest()  # Вычисляем хэш-сумму cfg файла
+                        if file_hash not in hash_table:
+                            dest_subdir = os.path.relpath(root, source_dir)  # Получаем относительный путь от исходной директории до текущей директории
+                            dest_path = os.path.join(dest_dir, dest_subdir, file)  # Формируем путь для копирования cfg файла
+                            if not os.path.exists(dest_path):
+                                os.makedirs(os.path.dirname(dest_path), exist_ok=True)  # Создаем все несуществующие директории для целевого файла
+                                shutil.copy2(file_path, dest_path)  # Копируем cfg файл в целевую директорию
+                            
+                            dat_dest_path = os.path.join(dest_dir, dest_subdir, dat_file)  # Формируем путь для копирования dat файла
+                            if not os.path.exists(dat_dest_path):
+                                os.makedirs(os.path.dirname(dat_dest_path), exist_ok=True)  # Создаем все несуществующие директории для целевого dat файла
+                                shutil.copy2(dat_file_path, dat_dest_path)  # Копируем dat файл в целевую директорию
+                                
+                            hash_table[file_hash] = (file, dest_path)  # Добавляем хэш-сумму файла в хэш-таблицу
+                            
+    hash_table_file_path = os.path.join(dest_dir, 'hash_table.json')  # Формируем путь для сохранения hash_table
+    with open(hash_table_file_path, 'w') as file:
+        json.dump(hash_table, file)  # Сохраняем hash_table в JSON файл
+
+def deleting_confidential_information_in_cfg_files(source_dir):
+    protected_files = []  # Создаем список для хранения путей к защищенным файлам
+    for root, dirs, files in os.walk(source_dir):  # Итерируемся по всем файлам и директориям в исходной директории
+        for file in files:  # Имя каждого файла
+            if file.endswith(".cfg"):  # Если файл имеет расширение .cfg
+                file_path = os.path.join(root, file)  # Получаем полный путь к cfg файлу
+                try:
+                    with open(file_path, 'r') as file:
+                        lines = file.readlines()
+                        parts = lines[0].split(',')
+                        if len(parts) >= 2:
+                            lines[0] = ",," + parts[-1].strip() + "\n"  # Удаляем информацию о локальной информации в cfg файле
+                    with open(file_path, 'w') as file:
+                        file.writelines(lines)
+                except:
+                    protected_files.append(file_path)  # Добавляем защищенный файл в список
+    with open(os.path.join(source_dir, 'protected_files.txt'), 'w') as file:
+        file.write('\n'.join(protected_files))  # Сохраняем список защищенных файлов в txt файл в корне папки
+
+# Пример использования функции
+# Путь к исходной директории
+source_directory = 'D:\Программирование\Fork\APS_Osc_sort\Отчёты' 
+# Путь к целевой директории
+destination_directory = 'D:\Программирование\Fork\APS_Osc_sort\Отчёты_копия'
+
+copy_cfg_and_dat_files(source_directory, destination_directory)
+# deleting_confidential_information_in_cfg_files(destination_directory)
+
+dest_dir = destination_directory
+hash_table_file_path = os.path.join(dest_dir, 'hash_table.json')  # Формируем путь к файлу с hash_table
+if os.path.exists(hash_table_file_path) and os.path.getsize(hash_table_file_path) > 0:
+    with open(hash_table_file_path, 'r') as file:
+        hash_table = json.load(file)  # Загружаем hash_table из JSON файла
+else:
+    hash_table = {}  # Создаем пустую хэш-таблицу
+print(hash_table)
