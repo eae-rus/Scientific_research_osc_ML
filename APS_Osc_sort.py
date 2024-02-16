9a31b5e56cb8dadebf3b618b4cb4eba1e7eb6bb6
Evdakov Aleksey <eae_rus@mail.ru>
2024-02-16 13:28:15 +0300

    Скорректирован алгоритм поиска осциллограмм

--- APS_Osc_sort.py
+++ APS_Osc_sort.py
@@ -4,8 +4,8 @@ import hashlib
 import json
 
 # Функция для обхода файловой системы
-def copy_cfg_and_dat_files(source_dir, dest_dir):
-    hash_table = {}  # Создаем хэш-таблицу для отслеживания скопированных файлов
+def copy_cfg_and_dat_files(source_dir, dest_dir, hash_table = {}):
+    # hash_table - хэш-таблицf для отслеживания скопированных файлов
     for root, dirs, files in os.walk(source_dir):  # Итерируемся по всем файлам и директориям в исходной директории
         for file in files:  # Имя каждого файла
             if file.endswith(".cfg"):  # Если файл имеет расширение .cfg
@@ -27,9 +27,9 @@ def copy_cfg_and_dat_files(source_dir, dest_dir):
                                 os.makedirs(os.path.dirname(dat_dest_path), exist_ok=True)  # Создаем все несуществующие директории для целевого dat файла
                                 shutil.copy2(dat_file_path, dat_dest_path)  # Копируем dat файл в целевую директорию
                                 
-                            hash_table[file_hash] = (file, dest_path)  # Добавляем хэш-сумму файла в хэш-таблицу
+                            hash_table[file_hash] = (file, file_path)  # Добавляем хэш-сумму файла в хэш-таблицу
                             
-    hash_table_file_path = os.path.join(dest_dir, 'hash_table.json')  # Формируем путь для сохранения hash_table
+    hash_table_file_path = os.path.join(dest_dir, '_hash_table.json')  # Формируем путь для сохранения hash_table
     try:
         with open(hash_table_file_path, 'w') as file:
             json.dump(hash_table, file)  # Сохраняем hash_table в JSON файл
@@ -61,5 +61,13 @@ source_directory = 'D:\Программирование\Fork\Scientific_research
 # Путь к целевой директории
 destination_directory = 'D:\Программирование\Fork\Scientific_research_osc_ML\Отчёты_копия'
 
-copy_cfg_and_dat_files(source_directory, destination_directory)
+hash_table = {}
+destination_directory_hash_table = destination_directory +  '/_hash_table.json'
+try:
+    with open(destination_directory_hash_table, 'r') as file:
+        hash_table = json.load(file)
+except:
+    print("Не удалось прочитать hash_table из JSON файла")
+
+copy_cfg_and_dat_files(source_directory, destination_directory, hash_table)
 # deleting_confidential_information_in_cfg_files(destination_directory)
