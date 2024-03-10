d91bc5ad7e031b841df16235ff9c9c74c8b23bc9
Aleksey Evdakov <eae_rus@mail.ru>
2024-03-10 23:32:24 +0300

    Модернезирована функция удаления конфеденциальной информации

--- APS_Osc_sort.py
+++ APS_Osc_sort.py
@@ -58,15 +58,36 @@ def deleting_confidential_information_in_cfg_files(source_dir):
             if file.endswith(".cfg"):  # Если файл имеет расширение .cfg
                 file_path = os.path.join(root, file)  # Получаем полный путь к cfg файлу
                 try:
-                    with open(file_path, 'r') as file:
+                    with open(file_path, 'r', encoding='utf-8') as file:
                         lines = file.readlines()
+                        # Удаляем информацию о локальной информации в cfg файле
                         parts = lines[0].split(',')
                         if len(parts) >= 2:
-                            lines[0] = ",," + parts[-1].strip() + "\n"  # Удаляем информацию о локальной информации в cfg файле
+                            lines[0] = ",," + parts[-1].strip() + "\n"
+                        # считываем количество сигналов
+                        signals, analog_signals, digital_signals = lines[1].split(',')
+                        signals = int(signals)
+                        # считываем параметры осциллограммы
+                        frequency = int(lines[signals + 2])
+                        frequency_discretization, count_point= lines[signals + 4].split(',')
+                        frequency_discretization, count_point = int(frequency_discretization), int(count_point)
+                        # перезапись даты
+                        new_date = '01/01/0001, 01:01:01.000000\n'
+                        lines[signals + 5] = new_date
+                        lines[signals + 6] = new_date
+                                                
                     with open(file_path, 'w') as file:
                         file.writelines(lines)
-                except:
+                    
+                    dat_file_path = file_path[:-4] + ".dat"  # Получаем полный путь к dat файлу 
+                    with open(dat_file_path, 'rb') as file: # решил делать по dat файлу, так как он точно отличается даже после всех корректировок
+                        file_hash = hashlib.md5(file.read()).hexdigest()  # Вычисляем хэш-сумму cfg файла
+                    os.rename(file_path, os.path.join(root, file_hash + '.cfg'))
+                    os.rename(dat_file_path, os.path.join(root, file_hash + '.dat'))
+                        
+                except Exception as e:
                     protected_files.append(file_path)  # Добавляем защищенный файл в список
+                    protected_files.append(f"Произошла ошибка при обработке cfg файла: {e}")
     with open(os.path.join(source_dir, 'protected_files.txt'), 'w') as file:
         file.write('\n'.join(protected_files))  # Сохраняем список защищенных файлов в txt файл в корне папки
 
