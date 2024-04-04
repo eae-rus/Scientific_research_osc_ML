fb04adba7a5b1ec422bf8c16ac4e5089dcbe1fb2
Evdakov Aleksey <eae_rus@mail.ru>
2024-04-04 17:38:48 +0300

    Исправление стилистики

--- file_preprocessing_functions.py
+++ file_preprocessing_functions.py
@@ -81,17 +81,17 @@ def diff_encoding(file_path, root, encoding_name): #FIXME: исправить н
         None
     """
     with open(file_path, 'r', encoding=encoding_name) as file:
-            lines = file.readlines()
-            # Удаляем информацию о локальной информации в cfg файле
-            parts = lines[0].split(',')
-            if len(parts) >= 2:
-                lines[0] = ",," + parts[-1].strip() + "\n"
-            # считываем количество сигналов
-            signals, analog_signals, digital_signals = lines[1].split(',')
-            signals = int(signals)
-            new_date = '01/01/0001, 01:01:01.000000\n'
-            lines[signals + 5] = new_date
-            lines[signals + 6] = new_date
+        lines = file.readlines()
+        # Удаляем информацию о локальной информации в cfg файле
+        parts = lines[0].split(',')
+        if len(parts) >= 2:
+            lines[0] = ",," + parts[-1].strip() + "\n"
+        # считываем количество сигналов
+        signals, analog_signals, digital_signals = lines[1].split(',')
+        signals = int(signals)
+        new_date = '01/01/0001, 01:01:01.000000\n'
+        lines[signals + 5] = new_date
+        lines[signals + 6] = new_date
 
     with open(file_path, 'w', encoding='utf-8') as file:
         for line in lines:
