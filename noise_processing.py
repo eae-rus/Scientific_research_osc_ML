d5c260e6709e9c614566f9324f8cd5ae62b49be2
Aleksey Evdakov <eae_rus@mail.ru>
2024-04-30 21:22:22 +0300

    Добавлен генератор дискретных имён

--- noise_processing.py
+++ noise_processing.py
@@ -101,6 +101,89 @@ def generate_analog_signals_name(is_print_to_console: bool = False, is_print_to_
     
     return dict_names
 
+def generate_discrete_signals_name(is_print_to_console: bool = False, is_print_to_json: bool = False, path_to_json_file: str = "") -> dict:
+    """
+    Функция генерирует группы со всеми именами сигналов
+    Вероятно, она пока не нужна.
+      
+    Args:
+        is_print_to_console (bool): печать на консоль
+        is_print_to_json (bool): печать в json файл
+        path_to_json_file (str): путь к json файлу
+
+    Returns:
+        dict - словарь с группами и именами сигналов
+    """
+    dict_names = {}
+    
+    dict_names_new = {}
+    dict_names_new["FABT"], dict_names_new["RNM"] = [], []
+    dict_names_new["FABT"].append("FABT | Bus-0 | operation")
+    dict_names_new["RNM"].append("RNM | Bus-0 | operation")
+    dict_names[f"Bus 0 (common signals)"] = dict_names_new
+    if is_print_to_console:
+        print(f"------------ секция 0 (общие сигналы) ------------")
+        print(dict_names_new)
+    
+    dict_names["Bus 0 (common signals)"] = dict_names_new
+    
+    for i in range(1, 9):
+        if is_print_to_console:
+            print(f"------------ секция {i} ------------")
+        # истинные данные
+        dict_names_new = {}
+        dict_names_new[f"PDR B{i}"] = []
+        dict_names_new[f"IFB B{i}"] = []
+        dict_names_new[f"BC B{i}"] = []
+        dict_names_new[f"BC Bypass B{i}"] = []
+        dict_names_new[f"FABT B{i}"] = []
+        dict_names_new[f"RNM B{i}"] = []
+
+        dict_names_new[f"PDR B{i}"].append(f"PDR | Bus-{i} | phase: A")
+        dict_names_new[f"PDR B{i}"].append(f"PDR | Bus-{i} | phase: B")
+        dict_names_new[f"PDR B{i}"].append(f"PDR | Bus-{i} | phase: C")
+        dict_names_new[f"PDR B{i}"].append(f"PDR | Bus-{i} | phase: PS") # Прямая последовательность или общий сигнал
+        
+        dict_names_new[f"IFB B{i}"].append(f"IFB | Bus-{i} | open")
+        dict_names_new[f"IFB B{i}"].append(f"IFB | Bus-{i} | close")
+        dict_names_new[f"IFB B{i}"].append(f"IFB | Bus-{i} | open | phase: A")
+        dict_names_new[f"IFB B{i}"].append(f"IFB | Bus-{i} | open | phase: B")
+        dict_names_new[f"IFB B{i}"].append(f"IFB | Bus-{i} | open | phase: C")
+        dict_names_new[f"IFB B{i}"].append(f"IFB | Bus-{i} | close | phase: A")
+        dict_names_new[f"IFB B{i}"].append(f"IFB | Bus-{i} | close | phase: B")
+        dict_names_new[f"IFB B{i}"].append(f"IFB | Bus-{i} | close | phase: C")
+        dict_names_new[f"IFB B{i}"].append(f"IFB | Bus-{i} | command open")
+        dict_names_new[f"IFB B{i}"].append(f"IFB | Bus-{i} | command close")
+        dict_names_new[f"IFB B{i}"].append(f"IFB | Bus-{i} | command open | phase: A")
+        dict_names_new[f"IFB B{i}"].append(f"IFB | Bus-{i} | command open | phase: B")
+        dict_names_new[f"IFB B{i}"].append(f"IFB | Bus-{i} | command open | phase: C")
+        dict_names_new[f"IFB B{i}"].append(f"IFB | Bus-{i} | command close | phase: A")
+        dict_names_new[f"IFB B{i}"].append(f"IFB | Bus-{i} | command close | phase: B")
+        dict_names_new[f"IFB B{i}"].append(f"IFB | Bus-{i} | command close | phase: C")
+        
+        dict_names_new[f"BC B{i}"].append(f"BC | Bus-{i} | open")
+        dict_names_new[f"BC B{i}"].append(f"BC | Bus-{i} | close")
+        dict_names_new[f"BC B{i}"].append(f"BC | Bus-{i} | command open")
+        dict_names_new[f"BC B{i}"].append(f"BC | Bus-{i} | command close")
+        
+        dict_names_new[f"BC Bypass B{i}"].append(f"BC Bypass | Bus-{i} | open")
+        dict_names_new[f"BC Bypass B{i}"].append(f"BC Bypass | Bus-{i} | close")
+        dict_names_new[f"BC Bypass B{i}"].append(f"BC Bypass | Bus-{i} | command open")
+        dict_names_new[f"BC Bypass B{i}"].append(f"BC Bypass | Bus-{i} | command close")
+        
+        dict_names_new[f"FABT B{i}"].append(f"FABT | Bus-{i} | operation")
+        dict_names_new[f"RNM B{i}"].append(f"RNM | Bus-{i} | operation")
+    
+        dict_names[f"Bus {i}"] = dict_names_new
+        if is_print_to_console:
+            print(dict_names_new)
+    
+    if is_print_to_json:
+        path = path_to_json_file + '/dict_discrete_names.json'
+        with open(path, "w") as file:
+            json.dump(dict_names, file)
+    return dict_names
+
 def generate_group_signals_from_csv(is_print_to_console: bool = False) -> dict:
     """
     Функция генерирует группы со всеми именами сигналов
@@ -385,6 +468,7 @@ except:
     print("Не удалось прочитать hash_table из JSON файла")
 
 test = generate_analog_signals_name(is_print_to_json=True, path_to_json_file='D:/DataSet/Для нормировки')
+test = generate_discrete_signals_name(is_print_to_json=True, path_to_json_file='D:/DataSet/Для нормировки')
 # test = generate_group_signals_from_csv(is_print_to_console = True)
 # noise_processing(source_directory, path_to_csv_file, is_use_qestion_1 = True, is_use_qestion_2 = True, is_use_qestion_3 = True)
 # set_base_values(source_directory, path_to_csv_file, base_values, osc_list)
