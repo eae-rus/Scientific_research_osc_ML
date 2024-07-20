f7441d21c5e3cac84c510f5d1cd8618342e94db9
Aleksey Evdakov <eae_rus@mail.ru>
2024-07-20 20:41:32 +0300

    Рефакторинг
    Произведено переименовывание переменных и лёгкое изменение структуры. Дорабатывать ещё много.

--- APS_Osc_sort.py
+++ APS_Osc_sort.py
@@ -9,21 +9,26 @@ import datetime
 # и затем опять же в командной строке
 # python -m auto_py_to_exe
 
-# Функция для обхода файловой системы
-def Search_and_copy_new_oscillograms(source_dir: str, dest_dir: str, hash_table: dict = {}, 
-                                     is_copy_saving_the_folder_structure: bool = True, is_use_hash: bool = True,
-                                     is_use_comtrade: bool = True, is_use_brs: bool = True) -> None:
-    """
-    Копирует файлы .cfg и соответствующие файлы .dat из исходного каталога в целевой каталог, отслеживая скопированные файлы.
+SOURCE_DIR = 'D:/DataSet/(копия)_ALL_OSC_v2'
+DEST_DIR = 'D:/DataSet/depersonalized_ALL_OSC_v2 (В процессе)'
+CFG_EXTENSION, DAT_EXTENSION, BRS_EXTENSION = '.cfg', '.dat', '.brs'
 
-    Функция проходит через исходный каталог, идентифицирует файлы. cfg, вычисляет их хэш-значения, 
-    копирует их в целевой каталог вместе с файлами .dat, обновляет хэш-таблицу скопированными файлами и сохраняет хэш-таблицы в JSON-файлах.
+
+# Функция для обхода файловой системы
+def copy_new_oscillograms(source_dir: str, dest_dir: str, copied_hashes: dict = {}, 
+                          preserve_dir_structure: bool = True, use_hashes: bool = True,
+                          use_comtrade: bool = True, use_brs: bool = True) -> None:
+    """
+    Копирует файлы осциллограмм из исходного каталога в целевой каталог, отслеживая скопированные файлы.
 
     Args:
         source_dir (str): путь к исходному каталогу.
         dest_dir (str): путь к целевому каталогу.
-        hash_table (dict): хэш-таблица для отслеживания скопированных файлов (по умолчанию-пустой словарь).
-        is_copy_saving_the_folder_structure (bool): сохранять ли структуру директорий в целевом каталоге?
+        copied_hashes (dict): хэш-таблица для отслеживания скопированных файлов (по умолчанию - пустой словарь).
+        preserve_structure (bool): сохранять ли структуру директорий в целевом каталоге?
+        use_hashes (bool): использовать ли хэш-суммы для проверки уникальности файлов?
+        copy_comtrade (bool): копировать ли файлы Comtrade (.cfg и .dat)?
+        copy_brs (bool): копировать ли файлы Bresler (.brs)?
 
     Returns:
         None.
@@ -32,23 +37,23 @@ def Search_and_copy_new_oscillograms(source_dir: str, dest_dir: str, hash_table:
         - создаётся новый файл "_new_hash_table"
     """
     # hash_table - хэш-таблица для отслеживания скопированных файлов
-    new_hash_table = {}
+    new_copied_hashes = {}
     count_new_files = 0
     for root, dirs, files in os.walk(source_dir):  # Итерируемся по всем файлам и директориям в исходной директории
         for file in files:  # Имя каждого файла
-            if is_use_comtrade and file.lower().endswith(".cfg"):  # Если файл имеет расширение .cfg
-                file = file[:-4] + ".cfg" # изменяем шрифт типа файла на строчный.
+            if use_comtrade and file.lower().endswith(CFG_EXTENSION):  # Если файл имеет расширение .cfg
+                file = file[:-4] + CFG_EXTENSION # изменяем шрифт типа файла на строчный.
                 file_path = os.path.join(root, file)  # Получаем полный путь к cfg файлу
-                dat_file = file[:-4] + ".dat"  # Формируем имя dat файла на основе имени cfg файла
+                dat_file = file[:-4] + DAT_EXTENSION  # Формируем имя dat файла на основе имени cfg файла
                 dat_file_path = os.path.join(root, dat_file)  # Получаем полный путь к dat файлу
                 is_exist = os.path.exists(dat_file_path)              
                 if is_exist:
                     with open(dat_file_path, 'rb') as f:  # Открываем dat файл для чтения в бинарном режиме
                         file_hash = hashlib.md5(f.read()).hexdigest()  # Вычисляем хэш-сумму dat файла
-                        if file_hash not in hash_table or not is_use_hash:
+                        if file_hash not in copied_hashes or not use_hashes:
                             dest_subdir = os.path.relpath(root, source_dir)  # Получаем относительный путь от исходной директории до текущей директории
                             
-                            if is_copy_saving_the_folder_structure:
+                            if preserve_dir_structure:
                                 dest_path = os.path.join(dest_dir, dest_subdir, file)  # Формируем путь для копирования cfg файла
                                 dat_dest_path = os.path.join(dest_dir, dest_subdir, dat_file)  # Формируем путь для копирования dat файла
                             else:
@@ -63,18 +68,18 @@ def Search_and_copy_new_oscillograms(source_dir: str, dest_dir: str, hash_table:
                                 os.makedirs(os.path.dirname(dat_dest_path), exist_ok=True)  # Создаем все несуществующие директории для целевого dat файла
                                 shutil.copy2(dat_file_path, dat_dest_path)  # Копируем dat файл в целевую директорию
                                 
-                            hash_table[file_hash] = (file, file_path)  # Добавляем хэш-сумму файла в хэш-таблицу
-                            new_hash_table[file_hash] = (file, file_path)
+                            copied_hashes[file_hash] = (file, file_path)  # Добавляем хэш-сумму файла в хэш-таблицу
+                            new_copied_hashes[file_hash] = (file, file_path)
                             count_new_files += 1
             
-            if is_use_brs and file.lower().endswith(".brs"):  # Если файл имеет расширение .brs (характерно для Бреслера)
-                file = file[:-4] + ".brs" # изменяем шрифт типа файла на строчный.
+            if use_brs and file.lower().endswith(BRS_EXTENSION):  # Если файл имеет расширение .brs (характерно для Бреслера)
+                file = file[:-4] + BRS_EXTENSION # изменяем шрифт типа файла на строчный.
                 file_path = os.path.join(root, file)  # Получаем полный путь к cfg файлу
                 with open(file_path, 'rb') as f: # Открываем brs файл для чтения в бинарном режиме
                     file_hash = hashlib.md5(f.read()).hexdigest()  # Вычисляем хэш-сумму dat файла
-                    if file_hash not in hash_table or not is_use_hash:
+                    if file_hash not in copied_hashes or not use_hashes:
                         dest_subdir = os.path.relpath(root, source_dir)  # Получаем относительный путь от исходной директории до текущей директории
-                        if is_copy_saving_the_folder_structure:
+                        if preserve_dir_structure:
                             dest_path_BRESELER = os.path.join(dest_dir,'BRESELER', dest_subdir, file)  # Формируем путь для копирования cfg файла
                         else:
                             dest_path_BRESELER = os.path.join(dest_dir,'BRESELER', file)  # Формируем путь для копирования cfg файла
@@ -82,94 +87,94 @@ def Search_and_copy_new_oscillograms(source_dir: str, dest_dir: str, hash_table:
                             os.makedirs(os.path.dirname(dest_path_BRESELER), exist_ok=True)  # Создаем все несуществующие директории для целевого файла
                             shutil.copy2(file_path, dest_path_BRESELER)  # Копируем файл в целевую директорию
                         
-                        if is_use_hash:  
-                            hash_table[file_hash] = (file, file_path)  # Добавляем хэш-сумму файла в хэш-таблицу
-                            new_hash_table[file_hash] = (file, file_path)
+                        if use_hashes:  
+                            copied_hashes[file_hash] = (file, file_path)  # Добавляем хэш-сумму файла в хэш-таблицу
+                            new_copied_hashes[file_hash] = (file, file_path)
                         count_new_files += 1
     
     print(f"Количество новых скопированных файлов: {count_new_files}") 
     # Сохранение JSON файлов hash_table и new_hash_table                        
     hash_table_file_path = os.path.join(dest_dir, '_hash_table.json')  # Формируем путь для сохранения hash_table
-    if is_use_hash: 
+    if use_hashes: 
         try:
             with open(hash_table_file_path, 'w') as file:
-                json.dump(hash_table, file)  # Сохраняем hash_table в JSON файл
+                json.dump(copied_hashes, file)  # Сохраняем hash_table в JSON файл
         except:
             print("Не удалось сохранить hash_table в JSON файл")
             
         data_now = datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S")
-        new_hash_table_file_path = os.path.join(dest_dir, f'new_hash_table_{data_now}.json')
+        new_copied_hashes_file_path = os.path.join(dest_dir, f'new_hash_table_{data_now}.json')
         try:
-            with open(new_hash_table_file_path, 'w') as file:
-                json.dump(new_hash_table, file)
+            with open(new_copied_hashes_file_path, 'w') as file:
+                json.dump(new_copied_hashes, file)
         except:
             print("Не удалось сохранить new_hash_table в JSON файл")
 
 
-def find_all_osc_for_terminal(dest_dir: str, hash_table: dict, osc_name_dict: dict) -> None:
+def match_oscillograms_to_terminals(dest_dir: str, copied_hashes: dict, terminal_oscillogram_names: dict) -> None:
     """
     Ищет коды осциллограмм в хэш-таблице и добавляет их в новый хэш-список.
       
     Args:
         dest_dir (str): каталог, для сохранения словаря осциллограмм.
-        hash_table (dict): хэш-таблица для отслеживания скопированных файлов.
-        osc_name_dict (dict): словарь для хранения кодов осциллограмм и их хэш-сумм.
+        copied_hashes (dict): хэш-таблица для отслеживания скопированных файлов.
+        terminal_oscillogram_names (dict): словарь для хранения кодов осциллограмм и их хэш-сумм.
     Returns:
         None
     """
     SCM_NAME = '\\ИПМ'
     new_osc_name_dict = {}
     new_osc_name_arr = [] # для ускорения работы циклов
-    for osc_name in osc_name_dict.keys():
+    for osc_name in terminal_oscillogram_names.keys():
         new_osc_name_arr.append(osc_name)
         new_osc_name_dict[osc_name] = '\\'+osc_name[1:]
         
     # Проходим по всем файлам в папке
-    for key in hash_table.keys():  # имён по хэш-суммам в разы больше, чем терминалов
+    for key in copied_hashes.keys():  # имён по хэш-суммам в разы больше, чем терминалов
         for osc_name in new_osc_name_arr:
-            if osc_name in hash_table[key][0]:
-                osc_name_dict[osc_name].append(key)
+            if osc_name in copied_hashes[key][0]:
+                terminal_oscillogram_names[osc_name].append(key)
                 break # если найдено имя осциллограммы, то прерываем цикл.
-            elif ('ОТГРУЖЕННЫЕ ТЕРМИНАЛЫ И ШКАФЫ' in hash_table[key][1] and new_osc_name_dict[osc_name] in hash_table[key][1] and 
-                  not SCM_NAME in hash_table[key][1]):
+            elif ('ОТГРУЖЕННЫЕ ТЕРМИНАЛЫ И ШКАФЫ' in copied_hashes[key][1] and new_osc_name_dict[osc_name] in copied_hashes[key][1] and 
+                  not SCM_NAME in copied_hashes[key][1]):
                 # сделано раздельно, чтобы отследить, что проверка добавлена не зря.
-                osc_name_dict[osc_name].append(key)
+                terminal_oscillogram_names[osc_name].append(key)
                 break
                 
     
     osc_name_dict_file_path = os.path.join(dest_dir, '_osc_name_dict.json')  # Формируем путь для сохранения osc_name_dict
     try:
         with open(osc_name_dict_file_path, 'w') as file:
-            json.dump(osc_name_dict, file)  # Сохраняем hash_table в JSON файл
+            json.dump(terminal_oscillogram_names, file)  # Сохраняем hash_table в JSON файл
     except:
         print("Не удалось сохранить osc_name_dict в JSON файл")
 
-def Copy_oscillogramms_for_terminals(source_dir: str, dest_dir: str, terminal_names: list, osc_name_dict: dict) -> None:
+def organize_oscillograms_by_terminal(source_dir: str, dest_dir: str, terminal_list: list, terminal_oscillogram_names: dict) -> None:
     """
     Копирует осциллограммы структуируя их по номерам терминалов.
       
     Args:
         source_dir (str): исходный каталог.
         dest_dir (str): каталог, для сохранения скопированных файлов.
-        terminal_names (list): список имён терминалов.
+        terminal_list (list): список имён терминалов.
         osc_name_dict (dict): словарь для хранения терминалов и хеш имён осциллограмм.
     Returns:
         None
     """
     osc_terminal_dict = {}  # словарь для хранения имён осциллограмм и их принадлежности к терминалу
     # Функция формирующая обратный список к словарю osc_name_dict (из "терминал -> имя осц" в "имя осц -> терминал")
-    for terminal_name in terminal_names:
-        for osc_name in osc_name_dict[terminal_name]:
+    for terminal_name in terminal_list:
+        for osc_name in terminal_oscillogram_names[terminal_name]:
             osc_terminal_dict[osc_name] = terminal_name
     
     for root, dirs, files in os.walk(source_dir):  # Итерируемся по всем файлам и директориям в исходной директории
         for file in files:  # Имя каждого файла
-            if file.endswith(".cfg") and file[:-4] in osc_terminal_dict:  # Если файл имеет расширение .cfg и есть в списке имён интересующих осциллограмм
+            if file.endswith(CFG_EXTENSION) and file[:-4] in osc_terminal_dict:  # Если файл имеет расширение .cfg и есть в списке имён интересующих осциллограмм
                 file_name = file[:-4]
                 cfg_file_name = file
-                dat_file_name = cfg_file_name[:-4] + ".dat"
+                dat_file_name = cfg_file_name[:-4] + DAT_EXTENSION
                 cfg_file_path = os.path.join(root, file)  # Формируем полный путь к cfg файлу
-                dat_file_path = cfg_file_path[:-3] + "dat"  # Формируем полный путь к dat файлу
+                dat_file_path = cfg_file_path[:-3] + DAT_EXTENSION  # Формируем полный путь к dat файлу
                 # копируем файлы в соответствующу папку
                 if os.path.exists(cfg_file_path) and os.path.exists(dat_file_path):
                     cfg_dest_path = os.path.join(dest_dir, osc_terminal_dict[file_name], cfg_file_name)  # Формируем путь для копирования dat файла
@@ -182,47 +187,41 @@ def Copy_oscillogramms_for_terminals(source_dir: str, dest_dir: str, terminal_na
                         os.makedirs(os.path.dirname(dat_dest_path), exist_ok=True)  # Создаем все несуществующие директории для целевого dat файла
                         shutil.copy2(dat_file_path, dat_dest_path)  # Копируем dat файл в целевую директорию              
 
+
 # Пример использования функции
-# Путь к исходной директории
-source_directory = 'D:/DataSet/_ALL_OSC_v2'
-# Путь к целевой директории
-destination_directory = 'D:/DataSet/depersonalized_ALL_OSC_v2.1'
 
-hash_table = {}
-destination_directory_hash_table = destination_directory +  '/_hash_table.json'
-
+copied_hashes = {}
+hash_table_path = DEST_DIR +  '/_hash_table.json'
 # source_directory = input("Введите путь в которой искать: ")
 # destination_directory = input("Введите путь в которую сохранять: ")
 # destination_directory_hash_table = input("Введите путь к папке с файлом '_hash_table.json': ")
 try:
-    with open(destination_directory_hash_table, 'r') as file:
-        hash_table = json.load(file)
+    with open(hash_table_path, 'r') as file:
+        copied_hashes = json.load(file)
 except:
     print("Не удалось прочитать hash_table из JSON файла")
 
 
-# Search_and_copy_new_oscillograms(source_directory, destination_directory, is_copy_saving_the_folder_structure=False, is_use_brs=False)
-# osc_name_dict = {}
-# osc_name_dict["t00108"], osc_name_dict["t00209"], osc_name_dict["t00331"], osc_name_dict["t00363"] = [], [], [], []
+copy_new_oscillograms(SOURCE_DIR, DEST_DIR, preserve_dir_structure=False, use_brs=False)
+# terminal_oscillogram_names = {}
+# terminal_oscillogram_names["t00108"], terminal_oscillogram_names["t00209"], terminal_oscillogram_names["t00331"], terminal_oscillogram_names["t00363"] = [], [], [], []
 # for i in range(1, 500): # пока лишь до 500, потом расширим
-#     osc_name_dict[f"t{i:05}"] = []
-# find_all_osc_for_terminal(destination_directory, hash_table, osc_name_dict)
-
-
+#     terminal_oscillogram_names[f"t{i:05}"] = []
+# match_oscillograms_to_terminals(destination_directory, copied_hashes, terminal_oscillogram_names)
 
 # Перенос файлов терминала в папку с осциллограммами
 osc_name_dict = {}
-destination_directory_osc_name_dict = destination_directory +  '/_osc_name_dict (по терминалам) v1.json'
+terminal_oscillogram_names_path = DEST_DIR +  '/_osc_name_dict (по терминалам) v1.json'
 # Путь к целевой директории
 dataset_for_primary_research_directory = 'D:/DataSet/dataset_for_primary_research'
 try:
-    with open(destination_directory_osc_name_dict, 'r') as file:
+    with open(terminal_oscillogram_names_path, 'r') as file:
         osc_name_dict = json.load(file)
 except:
     print("Не удалось прочитать hash_table из JSON файла")
-terminal_names = []
+terminal_list = []
 for i in range(1, 100): # пока лишь до 100, потом расширим
     terminal_name = f"t{i:05}"
-    terminal_names.append(terminal_name)
-Copy_oscillogramms_for_terminals(destination_directory, dataset_for_primary_research_directory, terminal_names, osc_name_dict)
+    terminal_list.append(terminal_name)
+# organize_oscillograms_by_terminal(destination_directory, dataset_for_primary_research_directory, terminal_list, osc_name_dict)
     
