758459fb33e9bdbb6f046295f05585d10069f12e
Evdakov Aleksey <eae_rus@mail.ru>
2024-08-07 15:45:23 +0300

    The "get_short_names_ml_signals" function has been adjusted

--- raw_to_csv/raw_to_csv.py
+++ raw_to_csv/raw_to_csv.py
@@ -300,7 +300,7 @@ class RawToCSV():
 
         return ml_signals
     
-    def get_short_names_ml_signals(self, use_operational_switching=True, use_abnormal_event=True, use_emergency_event=True):
+    def get_short_names_ml_signals(self, use_operational_switching: bool =True, use_abnormal_event: bool = True, use_emergency_event: bool = True) -> list:
         """
         This function returns a set of short names ML signals for (without i_bus).
 
@@ -310,59 +310,47 @@ class RawToCSV():
             use_emergency_event (bool): Include emergency event signals.
 
         Returns:
-            set: A set of ML signals for the given bus.
+            list: A list of ML signals for the given bus.
         """
         # FIXME: rewrite so that it is recorded at the very beginning and counted 1 time, and not at every request
 
-        ml_operational_switching = {
+        ml_operational_switching = [
             #--- Working switching ---
-            f'ML_1',      # Working switching, without specification
-            f'ML_1_1',    # Operational activation, without specification
-            f'ML_1_1_1',  # Operating start-up, engine start-up
-            f'ML_1_2',    # Operational shutdown, without specification
-        }
-        ml_abnormal_event = {
+            'ML_1',      # Working switching, without specification
+            'ML_1_1',    # Operational activation, without specification
+            'ML_1_1_1',  # Operating start-up, engine start-up
+            'ML_1_2',    # Operational shutdown, without specification
+        ]
+        ml_abnormal_event = [
             # --- Abnormal events
-            f'ML_2',      # Аномалия, без уточнения
-            f'ML_2_1',    # Однофазное замыкание на землю (ОЗЗ), без уточнения
-            f'ML_2_1_1',  # Устойчивое ОЗЗ
-            f'ML_2_1_2',  # Устойчивое затухающее ОЗЗ, с редкими пробоями
-            f'ML_2_1_3',  # Дуговое перемежающее однофазное замыкание на землю (ДПОЗЗ)
-            f'ML_2_2',    # Затухающие колебания от аварийных процессов
-            f'ML_2_3',    # Просадка напряжения
-            f'ML_2_3_1',  # Просадка напряжения при пуске двигателя
-            f'ML_2_4',    # Колебания тока, без уточнения
-            f'ML_2_4_1',  # Колебания тока при пуске двигателя
-            f'ML_2_4_2',  # Колебания тока, от двигателей с частотным приводом
-            
-            f'ML_2',      # Anomaly, without clarification
-            f'ML_2_1',    # Single phase-to-ground fault, without specification
-            f'ML_2_1_1',  # Sustainable single phase-to-ground fault
-            f'ML_2_1_2',  # Steady attenuating single phase-to-ground fault, with rare breakouts
-            f'ML_2_1_3',  # Arc intermittent single phase-to-ground fault
-            f'ML_2_2',    # Damping fluctuations from emergency processes
-            f'ML_2_3',    # Voltage drawdown
-            f'ML_2_3_1',  # Voltage drawdown when starting the engine
-            f'ML_2_4',    # Current fluctuations, without specification
-            f'ML_2_4_1',  # Current fluctuations when starting the engine
-            f'ML_2_4_2',  # Current fluctuations from frequency-driven motors
-            }
+            'ML_2',      # Anomaly, without clarification
+            'ML_2_1',    # Single phase-to-ground fault, without specification
+            'ML_2_1_1',  # Sustainable single phase-to-ground fault
+            'ML_2_1_2',  # Steady attenuating single phase-to-ground fault, with rare breakouts
+            'ML_2_1_3',  # Arc intermittent single phase-to-ground fault
+            'ML_2_2',    # Damping fluctuations from emergency processes
+            'ML_2_3',    # Voltage drawdown
+            'ML_2_3_1',  # Voltage drawdown when starting the engine
+            'ML_2_4',    # Current fluctuations, without specification
+            'ML_2_4_1',  # Current fluctuations when starting the engine
+            'ML_2_4_2',  # Current fluctuations from frequency-driven motors
+            ]
 
-        ml_emergency_event ={
+        ml_emergency_event = [
             # --- Emergency events ----
-            f'ML_3',      # Emergency events, without clarification
-            f'ML_3_1',    # An accident due to incorrect operation of the device, without clarification
-            f'ML_3_2',    # Terminal malfunction
-            f'ML_3_3'     # Two-phase earth fault
-        }
-
-        ml_signals = set()
+            'ML_3',      # Emergency events, without clarification
+            'ML_3_1',    # An accident due to incorrect operation of the device, without clarification
+            'ML_3_2',    # Terminal malfunction
+            'ML_3_3'     # Two-phase earth fault
+        ]
+        
+        ml_signals = []
         if use_operational_switching:
-            ml_signals.update(ml_operational_switching)
+            ml_signals.extend(ml_operational_switching)
         if use_abnormal_event:
-            ml_signals.update(ml_abnormal_event)
+            ml_signals.extend(ml_abnormal_event)
         if use_emergency_event:
-            ml_signals.update(ml_emergency_event)
+            ml_signals.extend(ml_emergency_event)
         
         return ml_signals, ml_operational_switching, ml_abnormal_event, ml_emergency_event
 
