b0ae97c1e8affd92cc630c01609e2416c98a9ffa
Evdakov Aleksey <eae_rus@mail.ru>
2024-10-22 16:22:10 +0300

    Added a neuromodel on complex numbers
    This is the first version of the model, and its modernization will be carried out in the future

--- ML_model/model.py
+++ ML_model/model.py
@@ -337,5 +337,101 @@ class FFT_MLP_KAN_v1(nn.Module):
         x = torch.cat((x_opr_swch, x_abnorm_evnt, x_emerg_evnt), dim=1)
         return x
 
+class ComplexLeakyReLU(nn.Module):
+    def forward(self, input, negative_slope = 0.05):
+        amp = torch.abs(input) # TODO: наверное можно как-то сразу, надо изучить, "что есть" в этих числах
+        # можно будет сделать порог сдвигаемым
+        amp_LeakyReLU = torch._C._nn.leaky_relu_(amp, negative_slope)
+        # позитив и негатив - для создания полуплоскости (можно будет сделать их адаптивными)
+        angle = torch.angle(input)
+        angle_LeakyReLU = torch._C._nn.leaky_relu_(angle, negative_slope)
+        
+        real = amp_LeakyReLU * torch.cos(angle_LeakyReLU)
+        imag = amp_LeakyReLU * torch.sin(angle_LeakyReLU)
+        return torch.complex(real, imag)
+
+class ComplexSigmoid(nn.Module):
+    # TODO: На будущее стоит сделать новый орган, который будет адаптивно учитывать границы угла, а не "зону"
+    # Актуально для выходной переменной
+    
+    def forward(self, input):
+        return torch.sigmoid(input.real)
+
+class FFT_MLP_COMPLEX_v1(nn.Module):
+    def __init__(
+        self, frame_size, channel_num=5, hidden_size=40, output_size=4, device = None,
+    ):
+        # TODO: разобраться в схеме обработки сигналов
+        # TODO: Подумать о создании более сложной схемы (2D свёртки, чтобы одновременно и I и U)
+        self.channel_num = channel_num
+        self.hidden_size = hidden_size
+        self.device = device
+        super(FFT_MLP_COMPLEX_v1, self).__init__()
+
+        # TODO: исправить расчётывание выходного размера после свёрток
+        # пока что задаю принудительно 128*16
+        self.fc = nn.Sequential(
+            nn.Linear(2*9*14, 4*hidden_size, dtype=torch.cfloat),
+            ComplexLeakyReLU(),
+            nn.Linear(4*hidden_size, 4*hidden_size, dtype=torch.cfloat),
+            ComplexLeakyReLU(),
+            nn.Linear(4*hidden_size, 2*hidden_size, dtype=torch.cfloat),
+            ComplexLeakyReLU(),
+            nn.Linear(2*hidden_size, hidden_size, dtype=torch.cfloat),
+            ComplexLeakyReLU(),
+        )
+        self.fc_opr_swch = nn.Sequential(
+            nn.Linear(hidden_size, hidden_size//2, dtype=torch.cfloat),
+            ComplexLeakyReLU(),
+            nn.Linear(hidden_size//2, 1, dtype=torch.cfloat),
+            ComplexSigmoid(),
+        )
+        self.fc_abnorm_evnt = nn.Sequential(
+
+            nn.Linear(hidden_size, hidden_size//2, dtype=torch.cfloat),
+            ComplexLeakyReLU(),
+            nn.Linear(hidden_size//2, 1, dtype=torch.cfloat),
+            ComplexSigmoid(),
+        )
+        self.fc_emerg_evnt = nn.Sequential(
+            nn.Linear(hidden_size, hidden_size//2, dtype=torch.cfloat),
+            ComplexLeakyReLU(),
+            nn.Linear(hidden_size//2, 1, dtype=torch.cfloat),
+            ComplexSigmoid(),
+        )
+
+    def fft_calc(self, input, count_harmonic=1):
+        # input - тензор формы (batch_size, channel_num, frame_size)
+        batch_size, channel_num, frame_size = input.size()
+        
+        # Вычисление FFT для каждого канала
+        fft_result_previous = torch.fft.rfft(input[:,:,:32])  # Одностороннее комплексное преобразование Фурье (работает с реальными числами)
+        fft_result_current = torch.fft.rfft(input[:,:,32:]) 
+
+        # Ограничиваем количество гармоник до count_harmonic + 1
+        fft_result_previous = fft_result_previous[:, :, :count_harmonic+1]
+        fft_result_current = fft_result_current[:, :, :count_harmonic+1]
+
+        # Объединяем в один тензор с последующим разворачиванием
+        fft_combined = torch.cat((fft_result_previous, fft_result_current), dim=-1)  # Склеиваем по последней размерности
+        
+        return fft_combined
+        
+    def forward(self, x):
+        ## ТРЕБУЕТСЯ сделать независимые выходы для КАЖДОГО класса 
+        x = x.reshape(x.size(0), x.size(2), x.size(1))
+        x = self.fft_calc(x, count_harmonic = 8)
+        # Concatenate tensors along axis 0
+        x = x.reshape(x.size(0), -1)
+        x = self.fc(x)
+        
+        # FEATURES_TARGET = ["opr_swch", "abnorm_evnt", "emerg_evnt"]
+        x_opr_swch = self.fc_opr_swch(x)
+        x_abnorm_evnt = self.fc_abnorm_evnt(x)
+        x_emerg_evnt = self.fc_emerg_evnt(x)
+        x = torch.cat((x_opr_swch, x_abnorm_evnt, x_emerg_evnt), dim=1)
+        return x
+
+
 if __name__ == "__main__":
     print(CONV_MLP_v2())
