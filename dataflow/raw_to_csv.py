4ea6d98160b64aaf9230ce7bd764a7c631785305
KovalenkoAE <98532402+KovalenkoAE@users.noreply.github.com>
2025-03-15 22:05:13 +0300

    Create raw_to_csv.py

---
+++ dataflow/raw_to_csv.py
@@ -0,0 +1,14 @@
+from dataflow.read_comtrade import ReadComtrade
+
+
+class RawToCSV():
+    """
+    This class implemented to convert raw comtrade files to csv file.
+    """
+    def __init__(self, raw_path='data/raw/', csv_path='data/csv', norm_coef_file_path='norm_coef.csv', uses_buses = ['1', '2', '12']):
+      
+    def create_csv(self, csv_name='datset.csv', is_cut_out_area = False, is_simple_csv = False):
+
+    def _create_one_df(self, file_path, file_name) -> pd.DataFrame:
+
+    def _split_buses(self, raw_df, file_name):
