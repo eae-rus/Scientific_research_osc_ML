7b5cfa8ae6fc26afcc67c19264ae6097f1483e8f
KovalenkoAE <98532402+KovalenkoAE@users.noreply.github.com>
2025-03-15 22:09:58 +0300

    Update raw_to_csv.py

--- dataflow/raw_to_csv.py
+++ dataflow/raw_to_csv.py
@@ -7,8 +7,10 @@ class RawToCSV():
     """
     def __init__(self, raw_path='data/raw/', csv_path='data/csv', norm_coef_file_path='norm_coef.csv', uses_buses = ['1', '2', '12']):
       
-    def create_csv(self, csv_name='datset.csv', is_cut_out_area = False, is_simple_csv = False):
+    def create_csv(self, csv_name='datset.csv', is_cut_out_area = False):
 
     def _create_one_df(self, file_path, file_name) -> pd.DataFrame:
 
     def _split_buses(self, raw_df, file_name):
+
+     def cut_out_area(self, buses_df: pd.DataFrame, samples_before: int, samples_after: int) -> pd.DataFrame:
